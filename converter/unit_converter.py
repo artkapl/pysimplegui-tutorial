174a8ab80abeb28b3b3151c9ce807d59e7509b57
Arthur <arthur.kaplan@gmx.at>
2024-02-17 16:22:39 +0100

    add conversion constants

--- converter/unit_converter.py
+++ converter/unit_converter.py
@@ -1,5 +1,10 @@
 import PySimpleGUI as sg
 
+### UNIT CONVERSIONS ###
+KM_TO_MILES = 0.6214
+KG_TO_POUNDS = 2.20462
+SEC_TO_MIN = 60
+
 layout = [
     [sg.Text('Simple Unit Converter')],
     [sg.Input(key='-INPUT-'), sg.Spin(['km to miles', 'kg to pound', 'sec to min'], key='-UNITS-'), sg.Button('Convert', key='-CONVERT-')],
@@ -29,13 +34,13 @@ while True:
         else:
             match values['-UNITS-']:
                 case 'km to miles':
-                    output = round(float(input) * 0.6214, 2)
+                    output = round(float(input) * KM_TO_MILES, 2)
                     output_str = f'{input} km = {output} miles'
                 case 'kg to pound':
-                    output = round(float(input) * 2.20462, 2)
+                    output = round(float(input) * KG_TO_POUNDS, 2)
                     output_str = f'{input} kg = {output} pounds'
                 case 'sec to min':
-                    output = round(float(input) / 60, 2)
+                    output = round(float(input) / SEC_TO_MIN, 2)
                     output_str = f'{input} seconds = {output} minutes'
 
         window['-RESULT-'].update(output_str)
